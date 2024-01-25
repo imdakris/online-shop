d8df3d13002f4d93ff2b96fc09472dc9e9a03728
Ruslan Dovhanych <imdakris@gmail.com>
2024-01-25 09:25:43 +0200

    add

--- main/views.py
+++ main/views.py
@@ -2,7 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index.html' )
+    context = {
+        'title': 'Home',
+        'content': 'Main page of the store - HOME'
+    }
+    return render(request, 'main/index.html', context )
 
 def about(request):
     return HttpResponse('About page')
