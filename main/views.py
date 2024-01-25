ca8f0d07785d7f1757b0a993b06f11b3c3743b03
Ruslan Dovhanych <imdakris@gmail.com>
2024-01-25 11:14:57 +0200

    add about.html

--- main/views.py
+++ main/views.py
@@ -3,12 +3,17 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Main page of the store - HOME'
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
     }
     return render(request, 'main/index.html', context )
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Statements must be separated by newlines or semicolonsPylance'
+    }
+    return render(request, 'main/about.html', context )
 
 
