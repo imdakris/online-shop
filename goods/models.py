1999c85cf58110e7ac52588a1de48f4648b052bc
Ruslan Dovhanych <imdakris@gmail.com>
2024-01-26 07:48:43 +0200

    create Products and Categories tables

--- goods/models.py
+++ goods/models.py
@@ -5,15 +5,12 @@ from django.db import models
 
 class Categories(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name="Название")
-    slug = models.SlugField(
-        max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
-    )
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name="URL")
 
     class Meta:
-        db_table = "category"
-        verbose_name = "Категорию"
+        db_table            = "category"
+        verbose_name        = "Категорию"
         verbose_name_plural = "Категории"
-
 
 class Products(models.Model):
     name        = models.CharField(max_length=150, unique=True, verbose_name="Название")
@@ -21,8 +18,11 @@ class Products(models.Model):
     description = models.TextField(blank=True, null=True, verbose_name="Описание")
     image       = models.ImageField(upload_to="goods_images", blank=True, null=True, verbose_name="Фото"    )
     price       = models.DecimalField(default=0.00,max_digits=7,decimal_places=2,verbose_name="Цена")
+    discount    = models.DecimalField(default=0.00,max_digits=7,decimal_places=2,verbose_name="Скидка в %")
+    quantity    = models.PositiveIntegerField(default=0, verbose_name="Количество")
+    category    = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name="Категория")
 
     class Meta:
-        db_table = "product"
-        verbose_name = "Товар"
-        verbose_name_plural = "Товары"
+        db_table            = "product"
+        verbose_name        = "Продукт"
+        verbose_name_plural = "Продукты"
