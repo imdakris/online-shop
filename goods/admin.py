757ff055cffc4d174a588763b819d606106e5015
Ruslan Dovhanych <imdakris@gmail.com>
2024-01-26 08:33:06 +0200

    Methods for fine-tuning the admin panel

--- goods/admin.py
+++ goods/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from goods.models import Categories, Products
 
-admin.site.register(Categories)
-admin.site.register(Products)
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    '''Methods for fine-tuning the admin panel'''
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Products)
+class CategoriesAdmin(admin.ModelAdmin):
+    '''Methods for fine-tuning the admin panel'''
+    prepopulated_fields = {'slug': ('name',)}
 
