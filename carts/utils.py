d8258061036e19dfc0d864cc341a511c0d617894
Ruslan Dovhanych <imdakris@gmail.com>
2024-01-31 09:17:40 +0200

    Sessions - cart for anonymous user

--- carts/utils.py
+++ carts/utils.py
@@ -2,5 +2,29 @@ from carts.models import Cart
 
 
 def get_user_carts(request):
+    """
+    Get the shopping carts associated with the current user or session.
+
+    Parameters:
+        - request: HttpRequest object.
+
+    Returns:
+        QuerySet: A QuerySet of Cart objects.
+
+    If the user is authenticated, the function returns the shopping carts associated with that user.
+    If the user is not authenticated, it checks for a session key. If no session key exists, it creates one.
+    The function then returns the shopping carts associated with the session.
+
+    Example:
+    ```python
+    user_carts = get_user_carts(request)
+    ```
+    """
     if request.user.is_authenticated:
         return Cart.objects.filter(user=request.user)
+    
+    if not request.session.session_key:
+        request.session.create()
+
+    return Cart.objects.filter(session_key=request.session.session_key)
+
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from turtle import update
-from django.contrib import messages
+from django.contrib import messages, sessions
 from django.http import JsonResponse
 from django.shortcuts import redirect
 from django.template.loader import render_to_string
@@ -24,6 +24,20 @@ def cart_add(request):
                 cart.save()
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
+
+    else:
+        carts = Cart.objects.filter(
+            session_key=request.session.session_key, product=product)
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(
+                session_key=request.session.session_key, product=product, quantity=1
+            )
+
     messages.success(request, f'Товар "{product.name}" добавлен в корзину.')
 
     user_cart = get_user_carts(request)
